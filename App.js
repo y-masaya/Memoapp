05f835dc004dc5ffe6872fd2d9c03a070b1343da
y-masaya <my.927ac@gmail.com>
2019-09-10 01:37:14 +0900

    Settings for eslint

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default function App extends Eeact.Componet{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>Hello!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
