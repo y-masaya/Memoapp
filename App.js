af0e8e8c07ad5f897e566858b036944d75548299
y-masaya <my.927ac@gmail.com>
2019-09-17 00:19:10 +0900

    Hide firebase certificate

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -7,15 +8,30 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+import ENV from './env.json';
+
+const firebaseConfig = {
+  apiKey:            ENV.FIREBASE_API_KEY,
+  authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL:       ENV.FIREBASE_DB_URL,
+  projectId:         ENV.FIREBASE_PRJ_ID,
+  storageBucket: '',
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId:             ENV.FIREBASE_APP_ID,
+};
+firebase.initializeApp(firebaseConfig);
+
 const App = createStackNavigator({
+  Login:         { screen: LoginScreen },
+  Signup:        { screen: SignupScreen },
   Home:          { screen: MemoListScreen },
   MemoDetail:    { screen: MemoDetailScreen },
   MemoEdit:      { screen: MemoEditScreen },
-  Login:         { screen: LoginScreen },
-  Signup:        { screen: SignupScreen },
 }, {
   defaultNavigationOptions: {
     headerTitle: 'Memot',
+    headerTintColor: '#fff',
+    headerBackTitle: null,
     headerStyle: {
       backgroundColor: '#265366',
     },
