479784dd5de12a4b2cf36c0df0f090d14646c329
y-masaya <my.927ac@gmail.com>
2019-09-17 03:56:31 +0900

    Signup and Login with Firebase

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import firebase from 'firebase';
 
 class LoginScreen extends React.Component {
   state = {
@@ -8,6 +9,14 @@ class LoginScreen extends React.Component {
   }
   // eslint-disable-next-line
   handleSubmit() {
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((user) => {
+        console.log('success!', user);
+        this.props.navigation.navigate('Home');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
